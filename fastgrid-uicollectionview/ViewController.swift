e4c741e35353f1230f6d84e6c9813a7a5d0a79e9
Volodymyr Pavliuk <gwotan@gmail.com>
2024-08-08 19:19:53 +0200

    Initial Commit

---
+++ fastgrid-uicollectionview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fastgrid-uicollectionview
+//
+//  Created by Vova on 08.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
